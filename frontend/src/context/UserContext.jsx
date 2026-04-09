3e74c4834f1422b0758073efbbbf154388bd121b
Aanchal Pal <aanchalpal2009@gmail.com>
2026-04-10 00:57:24 +0530

    fix production error

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -7,7 +7,7 @@ import axiosRateLimit from 'axios-rate-limit'
 
 export const userDataContext = createContext()
 function UserContext({ children }) {
-  const serverUrl = import.meta.env.BACKEND_URL || "http://localhost:8000"
+  const serverUrl = import.meta.env.VITE_BACKEND_URL || "http://localhost:8000"
   const [userData, setUserData] = useState(null)
   const [frontendImage, setFrontendImage] = useState(null)
   const [backendImage, setBackendImage] = useState(null)
